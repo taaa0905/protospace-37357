aa5080b44336a430f2e068eb1d21797a22aa37d9
taaa0905 <takashinakano0905@gmail.com>
2022-01-24 10:38:05 +0900

    ユーザー登録必須機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_many :prototypes
 
   validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
 end
